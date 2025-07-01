fca47aaaa00459b4b143bdb88bd7ab3caa1ef9f5
“Tanay <tansy.purwar@Zinnia.com>
2025-07-01 20:19:57 +0530

    working build

--- src/test/java/com/example/workflow/SampleProcessTest.java
+++ src/test/java/com/example/workflow/SampleProcessTest.java
@@ -1,53 +1,39 @@
-// package com.example.workflow;
+package com.example.workflow;
 
-// import org.camunda.bpm.engine.RuntimeService;
-// import org.camunda.bpm.engine.runtime.ProcessInstance;
-// import org.camunda.bpm.engine.test.Deployment;
-
-// import org.camunda.bpm.scenario.ProcessScenario;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.RegisterExtension;
-// import org.mockito.Mock;
-// import org.junit.jupiter.api.BeforeEach;
-
-// import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-
-// import org.camunda.bpm.engine.ProcessEngine;
-// import org.camunda.bpm.engine.ProcessEngineConfiguration;
-// import org.camunda.bpm.engine.TaskService;
-// import org.camunda.bpm.engine.impl.history.HistoryLevel;
-// import org.camunda.bpm.engine.variable.Variables;
-// import static org.mockito.Mockito.*;
-// import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.camunda.bpm.engine.TaskService;
 
 
 
-//  @Deployment(resources = "process.bpmn")
-// public class SampleProcessTest {
-//     //set history level to activity`
-//       public ProcessEngine myProcessEngine = ProcessEngineConfiguration
-//       .createStandaloneInMemProcessEngineConfiguration()
-//       .setJdbcUrl("jdbc:h2:mem:camunda;DB_CLOSE_DELAY=1000")
-//       .buildProcessEngine();
-      
-//   private ProcessScenario insuranceApplication = mock(ProcessScenario.class);
+
+ @Deployment(resources = "process.bpmn")
+public class SampleProcessTest {
 
 
-//       @BeforeEach
-//     public void defineHappyScenario() {
-//         when(insuranceApplication.waitsAtUserTask("say-hello")).thenReturn((task) -> task.complete(withVariables("approved", true)));
+  @Rule
+  public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+  
 
-//     }
+  @Test
+  public void ruleUsageExample() {
     
-//   @Test
-   
-//     void testHappyPath() {
-        
-//         RuntimeService runtimeService = extension.getProcessEngine().getRuntimeService();
-//         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process-design-patterns-process");
-//        assertThat(processInstance).isNotNull();
-//         assertThat(processInstance).isEnded();
-//     }
-// }
+    RuntimeService runtimeService = processEngineRule.getRuntimeService();
+    runtimeService.startProcessInstanceByKey("process-design-patterns-process");
+
+    TaskService taskService = processEngineRule.getTaskService();
+    Task task = taskService.createTaskQuery().singleResult();
+    assertEquals("Say hello to\nadmin", task.getName());
+
+    taskService.complete(task.getId());
+    assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+  }
+
+}
 
 
